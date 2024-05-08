9e9fd3edb30bcbed52154ae954cbeb360ee63224
Cyr73 <ckonst@mail.ru>
2024-05-08 09:05:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 # вывод списка ПК подключенных к базам 1С согласно журналу IIS
 from datetime import datetime
-import socket, os.path
+import socket, os.path, time
+
+def get_client_name(device_name):
+    return clients[device_name.lower()]
 
 def get_device_name(ip_address):
     try:
@@ -11,11 +14,11 @@ def get_device_name(ip_address):
     except socket.herror:
         return None
 
-today=datetime.now().strftime('%y%m%d')
+today = datetime.now().strftime('%y%m%d')
 filename ='C:/inetpub/logs/LogFiles/W3SVC1/u_ex'+today+'.log'
 if not os.path.isfile(filename):
     print ('никто сегодня не подключался через IIS')
-    input() # ожидание нажатия любой кнопки
+    time.sleep(3)# ожидание
     exit()
 items={} #Словарь
 # Чтение лог-файла и загрузка его содержимого
@@ -24,16 +27,17 @@ with open(filename, 'r') as f:
         if line.startswith('#'): continue
         data = line.split()
         URL = data[4]
-        base= URL.split('/')[1]
-        IP =  data[8]
+        base = URL.split('/')[1].lower()
+        IP = data[8]
         if 'login' in URL: 
-            items[base,IP]='login'    
+            if (base,IP) not in items: items[base,IP]=1
+            else: items[base,IP] +=1
         elif 'logout' in URL:
-            items[base,IP]='logout'
-for i in items:
-    if items[i]=='login':
+            if (base,IP) in items: items[base,IP] -=4
+            if items[base,IP]<=0: del items[base,IP] 
+for i in sorted(items):
         base=i[0]
         IP = i[1]  
         print(base, get_device_name(IP))
- 
-input() # ожидание нажатия любой кнопки
+print(' Всего:', len(items))
+time.sleep(15)# ожидание
